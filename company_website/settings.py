740a9bb5b671ba8327d8a28cd365456fd24bd195
Khamidkhanov Muhammadzohid <valihon555@gmail.com>
2025-10-24 16:53:00 +0500

    fixed

--- company_website/settings.py
+++ company_website/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from django.utils.translation import gettext_lazy as _
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -108,6 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
+LANGUAGE_CODE = 'uz'
 
 LANGUAGES = [
     ('uz', 'Uzbek'),
